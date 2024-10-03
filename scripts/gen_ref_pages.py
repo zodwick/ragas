30f19f768c29ef2d6a6ba8ae98e1bbaf2e7e9f77
zodwick <115868723+zodwick@users.noreply.github.com>
2024-10-03 21:05:08 +0530

    test branch for vercel deployment

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -32,7 +32,7 @@ for path in sorted(src.rglob("*.py")):
     nav[parts] = doc_path.as_posix()
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:
         identifier = ".".join(parts)
-        print("::: " + identifier, file=fd)
+        # print("::: " + identifier, file=fd)
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
 
